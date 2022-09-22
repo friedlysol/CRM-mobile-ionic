25001c56f6d0a05042b9214c72c985a1d95b1dc8
AdamZych333 <adam.zych99@wp.pl>
2022-09-22 10:54:44 +0200

    added saving/reading signature

--- src/app/components/signature/signature.component.ts
+++ src/app/components/signature/signature.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FileInterface } from '@app/interfaces/file.interface';
+import { FileService } from '@app/services/file.service';
 import { IonModal} from '@ionic/angular';
 import { ISignatureFromData } from './signature-form/signature-form.component';
 
@@ -12,19 +14,24 @@ export class SignatureComponent implements OnInit {
   @ViewChild('canvasModal') canvasModal: IonModal;
   @Input() objectType: string;
   @Input() objectUuid: string;
-  @Input() objectId?: number;
   @Input() typeId: number;
+  @Input() objectId?: number;
   @Input() label?: string;
 
+  file: FileInterface;
   ownerName = '';
   ownerTitle = '';
   image: string;
 
-  constructor() { }
+  constructor(
+    private fileService: FileService,
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.readSignature();
+  }
 
-  onClick(){
+  onGetSignatureClick(){
     this.formModal.present();
   }
 
@@ -51,13 +58,37 @@ export class SignatureComponent implements OnInit {
     })
     this.onCanvasCancel()
     this.onFormCancel();
+    this.saveSignature();
   }
 
   readSignature(){
-
+    this.fileService.getLastByObjectAndType(
+      this.objectType,
+      this.objectUuid,
+      this.typeId,
+    ).then((file) => {
+      this.file = file;
+    })
   }
 
   saveSignature(){
+    const file: FileInterface = {
+      object_type: this.objectType,
+      object_uuid: this.objectUuid,
+      object_id: this.objectId,
+      type_id: this.typeId,
+      description: `${this.ownerName}_${this.ownerTitle}`,
+      crc: this.image,
+      sync: 0,
+      created_at: new Date().toLocaleTimeString(),
+      path: '',
+      type: 'signature',
+    }
 
+    this.fileService.saveBase64File(
+      this.image,
+      `signature_${file.object_id}_${file.description}_${file.created_at}`,
+      file,
+    )
   }
 }
--- src/app/interfaces/file.interface.ts
+++ src/app/interfaces/file.interface.ts
@@ -1,22 +1,22 @@
 export interface FileInterface {
-  crc: string;
-  created_at: string;
-  description: string;
-  download_attempts: number;
-  gps_coords: string;
-  hash: string;
-  id: number;
-  is_deleted: number | boolean;
-  is_downloaded: number | boolean;
-  link_person_wo_id: number;
-  object_id: number;
+  crc?: string;
+  created_at?: string;
+  description?: string;
+  download_attempts?: number;
+  gps_coords?: string;
+  hash?: string;
+  id?: number;
+  is_deleted?: number | boolean;
+  is_downloaded?: number | boolean;
+  link_person_wo_id?: number;
+  object_id?: number;
   object_type: string;
   object_uuid: string;
-  path: string;
-  sync: number;
-  thumbnail: string;
+  path?: string;
+  sync?: number;
+  thumbnail?: string;
   type: string;
   type_id: number;
-  updated_at: string;
-  uuid: string;
+  updated_at?: string;
+  uuid?: string;
 }
