4ebf380528cb7497cb0a61a5865153e209b9fc19
Kamil Ciszewski <ckamil@poczta.fm>
2022-10-19 12:16:17 +0200

    AHD-93 - fixed query

---
+++ src/app/interfaces/time-sheet.interface.ts
@@ -0,0 +1,21 @@
+export interface TimeSheetInterface {
+  uuid?: string;
+  id?: number;
+  type_id: number;
+  vehicle_id?: number;
+  start_at: string;
+  stop_at?: string;
+  start_gps: string;
+  stop_gps?: string;
+  description?: string;
+  object_type: string;
+  object_uuid: string;
+  work_order_number?: string;
+  changed_in_crm?: number;
+  change_reason?: string;
+  hash?: string;
+  sync?: number;
+  auto_close_at?: string;
+  created_at?: string;
+  updated_at?: string;
+}
