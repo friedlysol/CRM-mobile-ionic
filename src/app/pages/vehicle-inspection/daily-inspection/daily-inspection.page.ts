be48e0bb98dc8c683f5259fb14a9dd3a953afc5a
Adam Zych <64246266+AdamZych333@users.noreply.github.com>
2022-10-20 13:01:09 +0200

    AHD-95

--- src/app/pages/vehicle-inspection/daily-inspection/daily-inspection.page.ts
+++ src/app/pages/vehicle-inspection/daily-inspection/daily-inspection.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-daily-inspection',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./daily-inspection.page.scss'],
 })
 export class DailyInspectionPage implements OnInit {
+  withoutPreview: 0 | 1;
+  redirectTo: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(async params => {
+      this.withoutPreview = params.get('withoutPreview') === '0'? 0: 1;
+      this.redirectTo = params.get('redirectTo');
+    });
   }
 
 }
